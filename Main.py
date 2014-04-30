51dcdc2c95f81f311671bfe999f2077f68e7d84b
Dpesqueda <dpesqueda@csumb.edu>
2014-04-30 15:43:15 -0700

    Playback & Recording Updates
    Changed the play and record functions to utilize PYO dsp classes.

--- Main.py
+++ Main.py
@@ -1,12 +1,5 @@
-'''
- Juan Hernandez
- CST205 
- Project 3
- '''
-
 from PyQt4 import QtGui, QtCore
-from PySide.QtCore import *
-from PySide.QtGui import *
+from pyo import *
 #import ExtendedQLabel
 import sys, os, shutil
 from Track import *
@@ -16,14 +9,22 @@ class Main(QtGui.QMainWindow):
     def __init__(self, parent = None):
         super(Main, self).__init__(parent)
 
-        # main button
+        # main buttons
         self.addButton = QtGui.QPushButton('ADD NEW TRACK')
-        self.addButton.clicked.connect(self.addTrackWidget)
+        self.addButton.setStyleSheet("background-color: #333377; border-radius:3;height:30;color:#ffffff;")
         self.stopButton = QtGui.QPushButton('STOP')
-        self.stopButton.setMaximumSize(250,100)
+        self.stopButton.setMaximumSize(200,160)
+        self.stopButton.setStyleSheet("background-color: #555544; border-radius:3;color:#ffffff;")
         self.playButton = QtGui.QPushButton('PLAY')
-        self.playButton.setMaximumSize(250,100)
+        self.playButton.setMaximumSize(200,160)
+        self.playButton.setStyleSheet("background-color: #445544; border-radius:3;color:#ffffff;")
+        self.recordButton = QtGui.QPushButton('RECORD')
+        self.recordButton.setMaximumSize(200,160)
+        self.recordButton.setStyleSheet("background-color: #554444; border-radius:3;color:#ffffff;")
 
+        # connect buttons to corresponding function
+        self.addButton.clicked.connect(self.addTrackWidget)
+        self.connect(self.recordButton, QtCore.SIGNAL('clicked()'), self.recordClicked)
         self.connect(self.playButton, QtCore.SIGNAL('clicked()'), self.playClicked)
         self.connect(self.stopButton, QtCore.SIGNAL('clicked()'), self.stopClicked)
 
@@ -41,13 +42,14 @@ class Main(QtGui.QMainWindow):
 
         # main layout
         self.mainLayout = QtGui.QVBoxLayout()
-        
+
         # Creat Top Sub Layout
         self.topSubLayout = QtGui.QHBoxLayout()
 
         #Top Layout Buttons 
         self.topSubLayout.addWidget(self.stopButton)
         self.topSubLayout.addWidget(self.playButton)
+        self.topSubLayout.addWidget(self.recordButton)
         self.topSubLayout.addWidget(self.addButton)
         
         self.mainLayout.addLayout(self.topSubLayout)
@@ -64,13 +66,21 @@ class Main(QtGui.QMainWindow):
         self.setCentralWidget(self.centralWidget)
         self.count = 0;
         
-        self.setGeometry(100,100,1000,600)
-        self.showMaximized()
+
 
         self.trackArray = {}
+        self.playArray = {} 
+        self.recordArray = {}
 
         # Dialog for creating project at start up
         self.projectPath = self.createProject()
+
+        self.play = Server().boot()
+        
+        self.setGeometry(100,100,1000,600)
+        self.showMaximized()
+
+        
    
 		
     def addTrackWidget(self):
@@ -93,6 +103,7 @@ class Main(QtGui.QMainWindow):
             self.createProjectDirs('temp')
             return 'temp'
 
+
     def createProjectDirs(self, dirName):
 
         if not os.path.exists(dirName):
@@ -102,33 +113,62 @@ class Main(QtGui.QMainWindow):
 
     def playClicked(self):
 
+        self.playButton.setStyleSheet("background-color: #118811; border-radius:3;color:#ffffff;")
+        self.playButton.setText('PLAYING')
         print "play clicked"
-        self.playIndex = 0
-        self.playArray = {} 
+
+        self.play.start()
+        
 
         for track in self.trackArray:
             if self.trackArray[track].getState() == 'active':
+                
                 print self.trackArray[track].getTrackName() + " is playing"
-                self.playArray[self.playIndex] = Play(self.trackArray[track].getTrackName())
-                self.playIndex+=1
 
-        for track in self.playArray:
-            self.playArray[track].start()
+                self.playback = SfPlayer(self.trackArray[track].getTrackName()).out()
 
     def stopClicked(self):
 
+        self.playButton.setStyleSheet("background-color: #445544; border-radius:3;color:#ffffff;")
+        self.playButton.setText('PLAY')
+        self.recordButton.setStyleSheet("background-color: #554444; border-radius:3;color:#ffffff;")
+        self.recordButton.setText('RECORD')
+        
         print "stop clicked"
+        self.play.stop()
+##        for track in self.playArray:
+##            self.playArray[track].terminate()
+##        for track in self.recordArray:
+##            self.recordArray[track].terminate()
 
-        for track in self.playArray:
-            self.playArray[track].terminate()
+    def recordClicked(self):
 
-    def stop_playback(self):
-        #subprocess.call(["killall","arecord"])  
-        self.play.terminate()
-        #self.show_wave_n_spec("test.wav")
+        if(len(self.trackArray) !=0):
+            self.recordButton.setStyleSheet("background-color: #881111; border-radius:3;color:#ffffff;")
+            self.recordButton.setText('RECORDING')
+            #print "play clicked"
+            self.playIndex = 0
+            self.recordIndex = 0
+            self.play.start()
+
+            for track in self.trackArray:
+                if self.trackArray[track].getState() == 'active':
+                    print self.trackArray[track].getTrackName() + " is playing"
+                    self.playArray[self.playIndex] = SfPlayer(self.trackArray[track].getTrackName()).out()
+                    self.playIndex+=1
+                if self.trackArray[track].getState() == 'record':
+                    print "recording on track "+self.trackArray[track].getTrackName() 
+                    self.recordArray[self.recordIndex] = Record(self.trackArray[track].getTrackName())
+                    self.recordIndex+=1
+
+            for track in self.recordArray:
+                self.recordArray[track].start()
+        else:
+            QtGui.QMessageBox.question(self, 'Warning!',"No tracks have been added.", QtGui.QMessageBox.Ok)
+
 
     def closeEvent(self, event):
-        #remove temp files
+        # remove temp files
         if(self.projectPath == 'temp'):
             shutil.rmtree('temp')
         # let the window close
@@ -137,6 +177,11 @@ class Main(QtGui.QMainWindow):
 
 app = QtGui.QApplication(sys.argv)
 myWidget = Main()
+
+p = myWidget.palette()
+p.setColor(QtGui.QPalette.Background,QtCore.Qt.black)
+myWidget.setPalette(p)
+
 myWidget.show()
 app.exec_()
 
